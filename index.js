2f730d1de8588edaafbcf76180d96232b758737b
Dmytro Iovenko <iovenko@gmail.com>
2024-08-06 14:31:56 -0400

    refactoring

--- index.js
+++ index.js
@@ -2,39 +2,8 @@ const csv = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,
 
 // const csv =
 //   "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232";
-let num_columns;
 
-// Declare a variable that stores the number of columns in each row of data within the CSV.
-// This should be calculated dynamically based on the first row of data.
-const rows = csv.split("\n");
-num_columns = rows[0].split(",").length;
-// console.log(num_columns);
-
-// Store your results in a two-dimensional array.
-// Each row should be its own array, with individual entries for each column.
-// Each row should be stored in a parent array, with the heading row located at index 0.
-// Cache this two-dimensional array in a variable for later use.
-const csvArr = []
-rows.forEach(row=>csvArr.push(row.split(",")))
-// console.log(csvArr);
-
-// For each row of data in the result array produced by your code above, create an object where the key of each value is the heading for that value’s column.
-// Convert these keys to all lowercase letters for consistency.
-// Store these objects in an array, in the order that they were originally listed.
-// Since the heading for each column will be stored in the object keys, you do not need to create an object for the heading row itself.
-const header = csvArr.shift();
-// console.log(header)
-// console.log(csvArr);
-const csvObjArr = []
-csvArr.forEach(rowArr=>{
-    let rowObj = {};
-    rowArr.forEach((cell, i)=>{
-        rowObj[header[i].toLowerCase()] = cell;
-    })
-    csvObjArr.push(rowObj)
-})
-console.log(csvObjArr);
-
+//// Part 1: Refactoring Old Code
 
 // console.log(rows);
 // for (let row of rows) {
@@ -63,3 +32,41 @@ console.log(csvObjArr);
 //   }
 //   console.log(cell1, cell2, cell3, cell4);
 // }
+
+
+//// Part 2: Expanding Functionality
+
+// Declare a variable that stores the number of columns in each row of data within the CSV.
+// This should be calculated dynamically based on the first row of data.
+let num_columns;
+const rows = csv.split("\n");
+num_columns = rows[0].split(",").length;
+// console.log(num_columns);
+
+// Store your results in a two-dimensional array.
+// Each row should be its own array, with individual entries for each column.
+// Each row should be stored in a parent array, with the heading row located at index 0.
+// Cache this two-dimensional array in a variable for later use.
+const csvArr = []
+rows.forEach(row=>csvArr.push(row.split(",")))
+// console.log(csvArr);
+
+//// Part 3: Transforming Data
+
+// For each row of data in the result array produced by your code above, create an object where the key of each value is the heading for that value’s column.
+// Convert these keys to all lowercase letters for consistency.
+// Store these objects in an array, in the order that they were originally listed.
+// Since the heading for each column will be stored in the object keys, you do not need to create an object for the heading row itself.
+const header = csvArr.shift();
+// console.log(header)
+// console.log(csvArr);
+const csvObjArr = []
+csvArr.forEach(rowArr=>{
+    let rowObj = {};
+    rowArr.forEach((cell, i)=>{
+        rowObj[header[i].toLowerCase()] = cell;
+    })
+    csvObjArr.push(rowObj)
+})
+// console.log(csvObjArr);
+
